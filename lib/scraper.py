7dc1634a6b67e223d2463205a1369e3bc0c5b19a
ussenuk <ukimanuka@gmail.com>
2024-03-04 17:45:30 +0300

    understanding webscraping

--- lib/scraper.py
+++ lib/scraper.py
@@ -3,4 +3,19 @@ from bs4 import BeautifulSoup
 import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
-html = requests.get("https://flatironschool.com/", headers=headers)
+html = requests.get("https://Moringaschool.com/", headers=headers)
+doc = BeautifulSoup(html.text, 'html.parser')
+
+print(doc.select('.heading-financier'))
+
+courses = doc.select('.heading-60-black.color-black.mb-20')
+
+for course in courses: 
+  print(course.contents[0].strip())
+
+  courses = doc.select('.heading-60-black.color-black.mb-20')[0]
+
+  name = doc.select('.heading-60-black.color-black.mb-20')[0].name
+
+  doc.select('.heading-60-black.color-black.mb-20')[0].attrs
+
